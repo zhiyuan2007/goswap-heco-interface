fd57aa6dcd4548a6cab70564b5bdd78e8309ef3f
fankouzu <1046166@qq.com>
2021-01-17 15:45:25 +0800

    bug fix

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -291,7 +291,7 @@ export default function FullPositionCard({ pair, border }: PositionCardProps) {
             <ButtonSecondary padding="8px" borderRadius="8px">
               <ExternalLink
                 style={{ width: '100%', textAlign: 'center' }}
-                href={`https://info.y3d.finance/account/${account}`}
+                href={`https://info.y3d.finance/account/${pair.liquidityToken}`}
               >
                 {t('viewAccruedFeesAndAnalytics')}
                 <span style={{ fontSize: '11px' }}>↗</span>
